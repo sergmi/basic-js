faf0407fb3bbe6438394b4aaf0bb73b97b65ca86
Sergej Miroshnikov <SVM1983@tut.by>
2020-03-22 18:16:16 +0200

    Basic-js/fix: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,12 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(Object.prototype.toString.call(arr).slice(8,-1) !== "Array"){throw "Error"; return;}
+   	arr.forEach(function(y,j,m){
+		switch(y) {
+			case "--discard-next": m.splice(j,2); break;
+			case "--discard-prev": m.splice(j-1,2); break;
+			case "--double-next": m.splice(j,1,m[j+1]); break;
+			case "--double-prev": m.splice(j,1,m[j-1]); break;
+		}
+	});
+	return arr;
 };
