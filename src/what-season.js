c8db058dd6bd7a4a7157213a94e7dcaf795e1317
Sergej Miroshnikov <SVM1983@tut.by>
2020-03-22 17:21:20 +0200

    Basic-js/fix: remove what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,9 +1,4 @@
-module.exports = function getSeason(date) {
-	if(Date.prototype.toString.call(date) === "Invalid Date"){throw "Invalid date"; return;}
-	
-	if(/Sep|Oct|Nov/.test(date) === true){return "autumn";}
-	else if(/Dec|Jan|Feb/.test(date) === true){return "winter";}
-	else if(/Mar|Apr|May/.test(date) === true){return "spring";}
-	else if(/Jun|Jul|Aug/.test(date) === true){return "summer";}
-	return;
+module.exports = function getSeason(/* date */) {
+  throw 'Not implemented';
+  // remove line with error and write your code here
 };
