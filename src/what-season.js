507cf66788f224e6355dc14b0a8595b404e94554
Sergej Miroshnikov <SVM1983@tut.by>
2020-03-22 16:23:27 +0200

    Basic-js/feature: solve what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,7 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+	if(/Sep|Oct|Nov/.test(date) === true){return "autumn";}
+	else if(/Dec|Jan|Feb/.test(date) === true){return "winter";}
+	else if(/Mar|Apr|May/.test(date) === true){return "spring";}
+	else if(/Jun|Jul|Aug/.test(date) === true){return "summer";}
+	return;
 };
