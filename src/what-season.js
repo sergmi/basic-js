0ce68563358940a26c7015d4d723241d7034afbb
Sergej Miroshnikov <SVM1983@tut.by>
2020-03-22 16:59:11 +0200

    Basic-js/fix: solve what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,6 @@
 module.exports = function getSeason(date) {
+	if(Date.prototype.toString.call(date) === "Invalid Date"){throw "Invalid date"; return;}
+	
 	if(/Sep|Oct|Nov/.test(date) === true){return "autumn";}
 	else if(/Dec|Jan|Feb/.test(date) === true){return "winter";}
 	else if(/Mar|Apr|May/.test(date) === true){return "spring";}
