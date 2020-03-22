018eb82f51e87a7ab03d679fa79e700aac76772d
Sergej Miroshnikov <SVM1983@tut.by>
2020-03-22 17:10:11 +0200

    Basic-js/fix: remove extended-repeater and simple-chain tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,46 +1,5 @@
-module.exports = function repeater(str,options) {
-    let res = "";
-
-	if(!options.repeatTimes){
-		options.repeatTimes = 1;
-	}
-	
-	if(!options.separator){
-		options.separator = "+";
-	}
-	
-	if(!options.addition){
-		options.addition = "";
-		options.additionRepeatTimes = 0;
-		options.additionSeparator = "";
-	}	
-	else{
-		if(!options.separator){
-			options.separator = "+";
-		}
-		if(!options.additionSeparator||options.additionRepeatTimes === 1){
-			options.additionSeparator = "";
-		}
-		if(!options.additionRepeatTimes){
-			options.additionRepeatTimes = 1;
-			options.additionSeparator = "";
-		}
-	}
-
-	let temp_addition;
-	if(options.addition){temp_addition = options.addition; options.addition = "";}
-
-	while(options.additionRepeatTimes){
-		options.additionRepeatTimes--;
-		options.addition += temp_addition;
-		if(options.additionRepeatTimes > 0){options.addition += options.additionSeparator;}
-	}
-	
-	while(options.repeatTimes){
-		options.repeatTimes--;
-		if(!options.repeatTimes){options.separator = "";}
-		res += str + options.addition + options.separator;
-	}
-
-	return res;
+module.exports = function repeater(/* str, options */) {
+    throw 'Not implemented';
+    // remove line with error and write your code here
 };
+  
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,66 +1,23 @@
 const chainMaker = {
   getLength() {
-     return this.length;
+    throw 'Not implemented';
+    // remove line with error and write your code here
   },
   addLink(value) {
-    let node = {value: value, next: null, prev: null}
-	let item;
-    	if(!this.head){
-		this.head = node;
-		this.tail = node;
-		this.length = 1;
-	}
-	else{
-		item = this.head;
-		while(item.next){
-			item = item.next;
-		}
-		node.prev = item;
-		item.next = node;
-		this.tail = item.next;
-		this.length++;
-	}
-	return this;
+    throw 'Not implemented';
+    // remove line with error and write your code here
   },
   removeLink(position) {
-		item = this.head;
-		while(position !== item.value && item.next){
-			item = item.next;
-				
-		}
-		
-		if(position === item.value){
-			if(!item.next&&item.prev){item.prev.next = null;}
-			else if(!item.prev){
-				if(!item.next){this.head = null;}
-				else{this.head = item.next;}
-			}
-			else{
-				item.next.prev = item.prev;
-				item.prev.next = item.next;
-			}
-			this.length--;
-		}
-		else{throw "value absent";}
-		
-		return this;
+    throw 'Not implemented';
+    // remove line with error and write your code here
   },
   reverseChain() {
     throw 'Not implemented';
     // remove line with error and write your code here
   },
   finishChain() {
-    let res = "";
-	if(!this.head){return false;}
-	else{
-		item = this.head;
-		res += "( " + item.value + " )";
-		while(item.next){
-			item = item.next;
-			res += "~~( " + item.value + " )";
-		}
-	}
-	return res;
+    throw 'Not implemented';
+    // remove line with error and write your code here
   }
 };
 
